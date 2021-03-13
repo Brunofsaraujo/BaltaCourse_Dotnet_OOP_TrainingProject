ab8772b63db608a879829374116e534f3367db3d
brunofsaraujo <brunofsaraujo@hotmail.com>
2021-03-13 20:55:08 -0300

    OOP: Trabalhando com listas

--- BaltaCourse_Dotnet_OOP_TrainingProject/ContentContext/Career.cs
+++ BaltaCourse_Dotnet_OOP_TrainingProject/ContentContext/Career.cs
@@ -2,6 +2,6 @@ namespace BaltaCourse_Dotnet_OOP_TrainingProject.ContentContext
 {
     public class Career : Content
     {
-
+        public string Courses { get; set; }
     }
 }
--- BaltaCourse_Dotnet_OOP_TrainingProject/ContentContext/Course.cs
+++ BaltaCourse_Dotnet_OOP_TrainingProject/ContentContext/Course.cs
@@ -1,7 +1,29 @@
+using System.Collections.Generic;
+
 namespace BaltaCourse_Dotnet_OOP_TrainingProject.ContentContext
 {
     public class Course : Content
     {
+        public Course()
+        {
+            Modules = new List<Module>();
+            Lectures = new List<Lecture>();
+        }
 
+        public string Tag { get; set; }
+        public IList<Module> Modules { get; set; }
+        public IList<Lecture> Lectures { get; set; }
+    }
+
+    public class Module
+    {
+        public int Order { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class Lecture
+    {
+        public int Order { get; set; }
+        public string Title { get; set; }
     }
 }
