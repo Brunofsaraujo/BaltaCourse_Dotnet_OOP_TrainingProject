55a8fc036b53a0b85df1950c38534d10d872c428
brunofsaraujo <brunofsaraujo@hotmail.com>
2021-03-13 20:30:10 -0300

    OOP: Criação projeto de treinamento

---
+++ BaltaCourse_Dotnet_OOP_TrainingProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaltaCourse_Dotnet_OOP_TrainingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
