252b8a6ce442b74b9d5b311f34050b3c33eb8d85
brunofsaraujo <brunofsaraujo@hotmail.com>
2021-03-13 21:04:47 -0300

    OOP: Enumeradores

---
+++ BaltaCourse_Dotnet_OOP_TrainingProject/ContentContext/Enums/EContentLevel.cs
@@ -0,0 +1,10 @@
+namespace BaltaCourse_Dotnet_OOP_TrainingProject.ContentContext.Enums
+{
+    public enum EContentLevel
+    {
+        Beginner = 1,
+        Fundamental = 2,
+        Intermediary = 3,
+        Advanced = 4
+    }
+}
