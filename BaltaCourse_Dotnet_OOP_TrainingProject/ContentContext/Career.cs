c7cc7280063c355e2cbb8d9081b0dd8f12311b90
brunofsaraujo <brunofsaraujo@hotmail.com>
2021-03-20 17:51:43 -0300

    OPP: Modelando a carreira

--- BaltaCourse_Dotnet_OOP_TrainingProject/ContentContext/Career.cs
+++ BaltaCourse_Dotnet_OOP_TrainingProject/ContentContext/Career.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+
 namespace BaltaCourse_Dotnet_OOP_TrainingProject.ContentContext
 {
     public class Career : Content
     {
-        public string Courses { get; set; }
+        public Career()
+        {
+            Items = new List<CareerItem>();
+        }
+
+        public IList<CareerItem> Items { get; set; }
+        public int TotalCourses => Items.Count;
     }
 }
